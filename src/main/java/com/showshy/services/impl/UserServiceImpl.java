f6331f0da88b3a6ccc6ab20f396417f3b5cf45f3
bogy <137620821@qq.com>
2017-03-28 20:00:29 +0800

    部分修改

--- src/main/java/com/showshy/services/impl/UserServiceImpl.java
+++ src/main/java/com/showshy/services/impl/UserServiceImpl.java
@@ -4,12 +4,14 @@ import com.showshy.mapper.UserMapper;
 import com.showshy.models.User;
 import com.showshy.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by Administrator on 2017-03-28.
  */
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     UserMapper userMapper;
